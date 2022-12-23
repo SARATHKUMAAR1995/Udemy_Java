0181b625f0408673aababd8cd080e7cc6cba89dd
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-23 10:13:48 +0530

    To find mb & kb using kb values

---
+++ src/CodingExercise/Ce03MegaBytesConverter.java
@@ -0,0 +1,18 @@
+package CodingExercise;
+
+public class Ce03MegaBytesConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(0);
+
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes >= 0) {
+            System.out.println(kiloBytes + " KB = " + (kiloBytes / 1024) + " MB and " + (kiloBytes % 1024) + " KB");
+        } else {
+            System.out.println("Invalid Value");
+        }
+
+
+    }
+}
