c5d8a1d64e4f3ddb6038bf0143b2704b60dee0a4
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-24 09:43:18 +0530

    Find centimeter using feet and inches value.

--- src/TeachingExercise/Te02MethodOverloading.java
+++ src/TeachingExercise/Te02MethodOverloading.java
@@ -3,6 +3,7 @@ package TeachingExercise;
 public class Te02MethodOverloading {
     public static void main(String[] args) {
 
+<<<<<<< HEAD
         System.out.println("68in = "+convertToCentimeters(68)+"cm");
         System.out.println("5ft, 8in = "+convertToCentimeters(5,8)+"cm");
 
@@ -14,5 +15,20 @@ public class Te02MethodOverloading {
         int feetToInches = feet * 12;
         int totalInches = feetToInches + inches;
         return (totalInches *2.54);
+=======
+        System.out.println("68in = " + convertToCentimeters(68) + "cm");
+        System.out.println("5ft, 8in = " + convertToCentimeters(5, 8) + "cm");
+
+    }
+
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+        int feetToInches = feet * 12;
+        int totalInches = feetToInches + inches;
+        return (totalInches * 2.54);
+>>>>>>> 5052302 (Find centimeter using feet and inches value.)
     }
 }
