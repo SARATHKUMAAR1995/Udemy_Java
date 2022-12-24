a36b8a93edc82fc3fa2695211b4792dabbdfe1f1
SARATH KUMAAR N <97964044+SARATHKUMAAR1995@users.noreply.github.com>
2022-12-24 09:43:18 +0530

    Find minutes and hours using seconds value

---
+++ src/TeachingExercise/Te02MethodOverloading.java
@@ -0,0 +1,18 @@
+package TeachingExercise;
+
+public class Te02MethodOverloading {
+    public static void main(String[] args) {
+
+        System.out.println("68in = "+convertToCentimeters(68)+"cm");
+        System.out.println("5ft, 8in = "+convertToCentimeters(5,8)+"cm");
+
+    }
+    public static double convertToCentimeters(int inches){
+        return inches * 2.54;
+    }
+    public static double convertToCentimeters(int feet, int inches){
+        int feetToInches = feet * 12;
+        int totalInches = feetToInches + inches;
+        return (totalInches *2.54);
+    }
+}
